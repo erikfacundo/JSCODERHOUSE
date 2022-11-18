fa9337838c2b63b16e6b327db2c9f516acca910f
erikfacundo <99147541+erikfacundo@users.noreply.github.com>
2022-11-18 11:16:38 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,59 +1,10 @@
-let presupuesto
-let aumento
-let precio
 
-// Clase persona para ingresar cliente
-class Personas {
-    constructor(nombre, apellido,) {
-        this.nombre = nombre;
-        this.apellido = apellido;
-    }
-}
+let clienteParse = JSON.parse(localStorage.getItem('Personas'));
+let message = document.getElementById('welcome');
+message.innerHTML += '¡Bienvenido ' + clienteParse[0].nombre + ' comenzemos con el prespuesto!';
 
-let persona = new Personas(Personas.nombre, Personas.apellido);
-
-persona.nombre = prompt("Ingrese su Nombre")
-while (persona.nombre == "") {
-    alert("¡Campo requerido!")
-    persona.nombre = prompt("Ingrese su nombre");
-}
-
-persona.apellido = prompt("Ingrese su apellido")
-while (persona.apellido == "") {
-    alert("¡Campo requerido!")
-    persona.apellido = prompt("Ingrese su apellido");
-};
-
-const nombreCliente = (persona.nombre + " " + persona.apellido);
-alert("Gracias por elegirnos " + nombreCliente +
-    `
-¡A continuacion te haremos algunas preguntas para tu presupuesto!`);
-
-let tratArray = []
-class tratamientos {
-    constructor(name, descripcion, precio, id) {
-        this.name = name;
-        this.descripcion = descripcion;
-        this.precio = precio
-        this.id = id
-    }
-}
-
-const Acrilico = new tratamientos("Acrilico", "Tratamiento Acrilico de 6 meses", 40000, 1,);
-tratArray.push(Acrilico);
-
-const Ceramico3 = new tratamientos("Ceramico3", "Tratamiento Cermico de 3 años", 60000, 2,);
-tratArray.push(Ceramico3);
-
-const Ceramico5 = new tratamientos("Ceramico5", "Tratamiento Cermico de 5 años", 80000, 3);
-tratArray.push(Ceramico5);
-
-const PPF = new tratamientos("PPF", "PPF completo 10 años de garantia", 100000, 4,);
-tratArray.push(PPF);
-
-
-// Ingresa el tamaño del vehiculo
-vehiculosArray = []
+//Creamos array de vehiculos//
+const vehiculosArray = []
 class vehiculo {
     constructor(name1, aumento, id) {
         this.name1 = name1;
@@ -72,66 +23,36 @@ vehiculosArray.push(suv);
 const pickup = new vehiculo('pickup', 1.30, 5)
 vehiculosArray.push(pickup);
 
-/*let vehiculoSeleccionado = parseInt(prompt(`Ingrese el vehiculo a tratar
-1- Chico
-2- Hatchback
-3- Sedan
-4 - Suv
-5- Pickup`))
+//Creamos array de tratamientos disponibles//
 
-console.log(vehiculoSeleccionado)*/
-let seleccion = prompt("Quiere realizar un tratamiento? si o no")
+let tratArray = []
+class tratamientos {
+    constructor(name, descripcion, precio, id) {
+        this.name = name;
+        this.descripcion = descripcion;
+        this.precio = precio
+        this.id = id
+    }    
+}    
 
-while (seleccion != "si" && seleccion != "no") {
+const Acrilico = new tratamientos("Acrilico", "Tratamiento Acrilico de 6 meses", 40000, 1,);
+tratArray.push(Acrilico);
 
-    alert("porfavor ingresa si o no")
-    seleccion = prompt("hola desa comprar algo si o no")
-}
+const Ceramico3 = new tratamientos("Ceramico3", "Tratamiento Cermico de 3 años", 60000, 2,);
+tratArray.push(Ceramico3);
 
-if (seleccion == "si") {
-    alert("Tratamiento disponibles")
-    let trataSeleccionado = tratArray.map
-        ((tratamiento) => tratamiento.id + " - " + tratamiento.name + "  " + tratamiento.precio);
+const Ceramico5 = new tratamientos("Ceramico5", "Tratamiento Cermico de 5 años", 80000, 3);
+tratArray.push(Ceramico5);
 
-    alert(trataSeleccionado.join("  "));
-} else if (seleccion == "no") {
-    alert("gracias por venir, hasta pronto")
-}
+const PPF = new tratamientos("PPF", "PPF completo 10 años de garantia", 100000, 4,);
+tratArray.push(PPF);
 
-let tratamiento = prompt("Seleccione un tratamiento")
+//Recorrer los array de vehiculos//
+let message1 = document.getElementById('tamanosDisponibles')
+let tamanosDisponibles = vehiculosArray.map
+((vehiculosArray) => vehiculosArray.id +  `  ` + vehiculosArray.name1 + `  ` + vehiculosArray.aumento);
+message1.innerHTML += 'Los tamaños de vehiculos disponibles son los siguientes ' + vehiculosArray ;
 
-if (tratamiento == "Acrilico" || tratamiento == "Ceramico3" || tratamiento == "Ceramico5" || tratamiento == "PPF") {
-    switch (tratamiento) {
-        case "Acrilico":
-            precio = 40000;
-            break;
-        case "Ceramico3":
-            precio = 60000;
-            break;
-        case "Ceramico5":
-            precio = 80000;
-            break;
-        case "PPF":
-            precio = 100000;
-            break;
-
-    
-    }
-}
-
-let seleccion2
-
-if (seleccion2 == "si") {
-    alert("Vehiculos disponibles")
-    let vehiculoSeleccionado = vehiculosArray.map
-        ((auto) => auto.id + " - " + auto.name1 + "  " + auto.aumento);
-
-    alert(vehiculoSeleccionado.join("  "));
-} else if (seleccion2 == "no") {
-    alert("gracias por venir, hasta pronto")
-}
-
-let auto = prompt("Seleccione el tamaño del vehiculo")
 
 if (auto == "chico" || auto == "hatchback" || auto == "sedan" || auto == "suv" || auto == "pickup") {
     switch (auto) {
@@ -148,18 +69,42 @@ if (auto == "chico" || auto == "hatchback" || auto == "sedan" || auto == "suv" |
             aumento = 1.30;
             break;
         case "pickup":
-            auemnto = 1.35;
+            aumento = 1.35;
             break;
-
+    }
+    }else{
+        alert("Seleccione opcion valida")
     }
 
-}else{
-    alert("no coincide")
+
+//Recorrer array de tratamientos//
+let message2 = document.getElementById('tratDisponible')
+let tratDisponible = tratArray.map
+((tratArray) => tratArray.id + `  ` +  tratArray.name + `  ` + tratArray.precio);
+message2.innerHTML += 'Los tratamientos disponibles son los siguientes ' + tratArray ;
+
+if (tratamiento == "Acrilico" || tratamiento == "Ceramico3" || tratamiento == "Ceramico5" || tratamiento == "PPF") {
+    switch (tratamiento) {
+        case "Acrilico":
+            precio = 40000;
+            break;
+        case "Ceramico3":
+            precio = 60000;
+            break;
+        case "Ceramico5":
+            precio = 80000;
+            break;
+        case "PPF":
+            precio = 100000;
+            break;
+
+
+    }
 }
 
+
 function presupuestoFinal() {
-    presupuesto = alert(precio * aumento);
-    console.log(presupuesto)
+    presupuesto = alert("Gracias por elegirnos  " + clienteParse + ". El costo del tratamiento es de " + "  " + precio * aumento + ". ");
 }
-//presupuesto.push({auto, tratamiento})
+
 presupuestoFinal();
